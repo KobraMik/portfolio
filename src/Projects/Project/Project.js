ca1a1da79fbd4c6cf54cec05c526fd8af1ff7218
Mike <slavik433@gmail.com>
2022-04-13 17:08:12 +0300

    completed block 'Projects'

--- src/Projects/Project/Project.js
+++ src/Projects/Project/Project.js
@@ -5,11 +5,17 @@ import style from "./Project.module.css";
 function Project(props) {
     return (
         <div className={style.project}>
-            <div className={style.image}>
+            <div className={style.image} style={props.style}>
                 <a className={style.buttonWatch}>Watch</a>
             </div>
-            <h3>{props.title}</h3>
-            <div className={style.description}>{props.description}</div>
+            <h3>
+                <a href="#">{props.title}</a>
+            </h3>
+            <div className={style.description}>
+                <p>
+                    {props.description}
+                </p>
+            </div>
         </div>)
 }
 
--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -6,14 +6,25 @@ import Title from "../common/components/title/Title";
 
 
 function Projects() {
+
+    const socialNetwork = {
+        backgroundImage: 'url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSy1nyLcMb-2Ia0NPxRmvnIYuqsKGJUA-pYVA&usqp=CAU")',
+    };
+
+    const todoList = {
+        backgroundImage: 'url("https://balancethroughsimplicity.com/wp-content/uploads/2020/04/How-to-write-a-To-Do-list-to-get-things-done-BLOG-1.jpg")',
+    }
+
     return (
         <div className={style.projectsBlock}>
             <div className={`${styleContainer.container} ${style.projectsContainer}`}>
                 <Title text={'Projects'}/>
-                <div className={style.projects}>
-                    <Project title={"Social network"}
+                <div className={style.projectsMain}>
+                    <Project style={socialNetwork}
+                             title={"Social network"}
                              description={"Social networkSocial networkSocial networkSocial networkSocial networkSocial networkSocial networkSocial network"}/>
-                    <Project title={"Todo list"}
+                    <Project style={todoList}
+                             title={"Todo list"}
                              description={"Todo listTodo listTodo listTodo listTodo listTodo "}/>
                 </div>
             </div>
